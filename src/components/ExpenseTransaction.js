acf4aabfc68ba9e2373178619b3b71330cd534d8
laherasif <laherasif@gmail.com>
2020-11-22 22:45:21 -0800

    initial commit

---
+++ src/components/ExpenseTransaction.js
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+
+const ExpenseTransaction = ({ expenseTransaction }) => {
+  const { deleteTransaction } = useContext(GlobalContext);
+
+  return (
+    <li className="transaction">
+      <span className="transaction-text">{expenseTransaction.expenseText}</span>
+      <span className="transaction-amount">
+        ${expenseTransaction.expenseAmount}
+      </span>
+      <button
+        onClick={() => deleteTransaction(expenseTransaction.id)}
+        className="delete-btn"
+      >
+       <i class="fa fa-trash" aria-hidden="true"></i>
+      </button>
+    </li>
+  );
+};
+
+export default ExpenseTransaction;
---
+++ src/components/IncomeTransaction.js
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+
+const IncomeTransaction = ({ incomeTransaction }) => {
+  const { deleteTransaction } = useContext(GlobalContext);
+
+  return (
+    <li className="transaction">
+      <span className="transaction-text">{incomeTransaction.incomeText}</span>
+      <span className="transaction-amount">
+        ${incomeTransaction.incomeAmount}
+      </span>
+      <button
+        onClick={() => deleteTransaction(incomeTransaction.id)}
+        className="delete-btn"
+      >
+         <i class="fa fa-trash" aria-hidden="true"></i>
+      </button>
+    </li>
+  );
+};
+
+export default IncomeTransaction;
